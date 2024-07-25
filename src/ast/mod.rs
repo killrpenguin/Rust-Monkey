6c7f21bde9d8760c026404d67b7d364ac1d1654a
David <dmcfarland8031@gmail.com>
2024-07-25 15:45:10 -0600

    Completed error handling with custom result.

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -4,9 +4,6 @@ pub mod monkey_ast {
     use std::fmt;
 
     #[derive(Debug, Clone, PartialEq)]
-    pub struct Ident(pub String);
-
-    #[derive(Debug, Clone, PartialEq)]
     pub struct Program {
         statement_nodes: Vec<Stmt>,
     }
@@ -22,26 +19,61 @@ pub mod monkey_ast {
             }
         }
     }
-
+    #[derive(Debug, Clone, PartialEq)]
+    pub struct Ident(pub String);
+    impl fmt::Display for Ident {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "Ident({})", &self.0)
+        }
+    }
     #[derive(Debug, Clone, PartialEq)]
     pub enum Stmt {
         Let(Expression, Expression),
         Return(Expression),
         ExprStmt(Expression, Option<Expression>),
     }
-
+    impl fmt::Display for Stmt {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match &self {
+                Stmt::Let(expr1, expr2) => write!(f, "Let(\n  {}\n  {})\n", expr1, expr2),
+                Stmt::Return(expr1) => write!(f, "Return(\n  {})\n", expr1),
+                Stmt::ExprStmt(expr1, expr2) => {
+                    write!(f, "(\n  {}\n  {})\n", expr1, expr2.as_ref().unwrap())
+                }
+            }
+        }
+    }
     #[derive(Debug, Clone, PartialEq)]
     pub enum Expression {
+        Prefix(PrefixOp, Box<Expression>),
         Infix(Operator, Box<Expression>),
         ExprIdent(Ident),
         Literal(Type),
     }
-
+    impl fmt::Display for Expression {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match &self {
+                Expression::Prefix(op, expr) => write!(f, "Prefix(\n  {}\n  {})\n", op, expr),
+                Expression::Infix(op, expr) => write!(f, "Infix(\n  {}\n  {})\n", op, expr),
+                Expression::Literal(lit) => write!(f, "Literal(\n  {})\n", lit),
+                Expression::ExprIdent(ident) => write!(f, "Ident(\n  {})\n", ident),
+            }
+        }
+    }
     #[derive(Debug, Clone, PartialEq)]
     pub enum Type {
         Int(usize),
         Float(f64),
         Bool(bool),
+    }
+    impl fmt::Display for Type {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match &self {
+                Type::Int(val) => write!(f, "Type: int\n  Value: {}", val),
+                Type::Float(val) => write!(f, "Type: float\n  Value: {}", val),
+                Type::Bool(val) => write!(f, "Type: bool\n  Value: {}", val),
+            }
+        }
     }
     #[derive(Debug, Clone, PartialEq)]
     pub enum Operator {
@@ -56,10 +88,9 @@ pub mod monkey_ast {
         LtEq,
         GtEq,
     }
-
     impl fmt::Display for Operator {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            match *self {
+            match &self {
                 Operator::Plus => write!(f, "+"),
                 Operator::Minus => write!(f, "-"),
                 Operator::Multiply => write!(f, "*"),
@@ -73,7 +104,21 @@ pub mod monkey_ast {
             }
         }
     }
-
+    #[derive(Debug, Clone, PartialEq, PartialOrd)]
+    pub enum PrefixOp {
+        Positive,
+        Negative,
+        NotEq,
+    }
+    impl fmt::Display for PrefixOp {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match &self {
+                PrefixOp::Positive => write!(f, "Positive."),
+                PrefixOp::Negative => write!(f, "Negative."),
+                PrefixOp::NotEq => write!(f, "Not Equal."),
+            }
+        }
+    }
     #[derive(Debug, Clone, PartialEq, PartialOrd)]
     pub enum Precedence {
         Lowest,
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -6,13 +6,69 @@ pub mod monkey_parser {
     use crate::ast::monkey_ast::*;
     use crate::lexer::monkey_lexer::*;
     use crate::token::tokens::*;
+    use std::{error, fmt};
+
+    type Result<'a, T> = std::result::Result<T, Box<dyn error::Error>>;
+
+    // std::any::type_name is unstable. Output may change.
+    // No guarantee that all parts of a type will appear in returned string.
+    macro_rules! get_fn_name {
+        () => {{
+            fn f() {}
+            fn type_name_of<T>(_: T) -> &'static str {
+                std::any::type_name::<T>()
+            }
+            let name = type_name_of(f);
+            name.strip_suffix("::f").unwrap()
+        }};
+    }
+
+    #[derive(Debug)]
+    pub struct MonkeyError<T: AsRef<str> + fmt::Debug + fmt::Display> {
+        err_in: T,
+    }
+
+    impl<T: AsRef<str> + std::fmt::Debug + fmt::Display> MonkeyError<T> {
+        pub fn new(f_name: T) -> MonkeyError<T> {
+            MonkeyError { err_in: f_name }
+        }
+    }
+
+    impl<T: AsRef<str> + fmt::Debug + fmt::Display> error::Error for MonkeyError<T> {}
+
+    impl<T: AsRef<str> + fmt::Debug + fmt::Display> fmt::Display for MonkeyError<T> {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "Parser Error in {}.", self.err_in)
+        }
+    }
+
+    #[derive(Debug)]
+    pub enum ErrorType {
+        InvalidToken(Box<(dyn error::Error + 'static)>, Option<Token>),
+    }
+
+    impl error::Error for ErrorType {}
+
+    impl From<Box<dyn error::Error>> for ErrorType {
+        fn from(error: Box<(dyn error::Error + 'static)>) -> ErrorType {
+            ErrorType::InvalidToken(error, None)
+        }
+    }
+    impl fmt::Display for ErrorType {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match self {
+                ErrorType::InvalidToken(err, tkn) => {
+                    write!(f, "{} Token::{} is Invalid.", err, tkn.clone().unwrap())
+                }
+            }
+        }
+    }
 
     pub struct Parser<'a> {
         lexer: Lexer<'a>,
         cur_token: Token,
         next_token: Token,
     }
-
     impl<'a> Parser<'a> {
         pub fn new(lexer: Lexer<'a>) -> Parser<'a> {
             let mut parser = Parser {
@@ -23,6 +79,13 @@ pub mod monkey_parser {
             parser.next_token();
             parser.next_token();
             parser
+        }
+
+        pub fn parser_err<T: AsRef<str> + fmt::Debug + fmt::Display + 'static>(
+            &self,
+            name: T,
+        ) -> Box<MonkeyError<T>> {
+            return Box::new(MonkeyError::new(name));
         }
 
         pub fn token_precedence(token: &Token) -> Precedence {
@@ -42,11 +105,11 @@ pub mod monkey_parser {
         }
 
         pub fn cur_token_is(&self, token: &Token) -> bool {
-            self.cur_token == *token
+            &self.cur_token == token
         }
 
         pub fn next_token_is(&self, token: &Token) -> bool {
-            self.next_token == *token
+            &self.next_token == token
         }
 
         pub fn next_token_if_is(&mut self, token: Token) -> Option<()> {
@@ -55,6 +118,22 @@ pub mod monkey_parser {
                 Some(())
             } else {
                 None
+            }
+        }
+
+        pub fn is_operator(&mut self) -> bool {
+            match self.cur_token {
+                Token::Plus => true,
+                Token::Minus => true,
+                Token::Asterisk => true,
+                Token::Slash => true,
+                Token::Eq => true,
+                Token::NotEq => true,
+                Token::Lt => true,
+                Token::Gt => true,
+                Token::LtEq => true,
+                Token::GtEq => true,
+                _ => return false,
             }
         }
 
@@ -70,152 +149,128 @@ pub mod monkey_parser {
             let mut program_stmts: Vec<Stmt> = vec![];
 
             while !self.next_token_is(&Token::Eof) {
-                if let Some(stmt) = self.parse_stmt() {
-                    program_stmts.push(stmt)
-                }
+                program_stmts.push(self.parse_stmt());
                 self.next_token()
             }
             program_stmts
         }
 
-        pub fn parse_stmt(&mut self) -> Option<Stmt> {
+        pub fn parse_stmt(&mut self) -> Stmt {
             match self.cur_token {
-                Token::Let => self.parse_let_stmt(),
-                Token::Ret => self.parse_ret_stmt(),
-                _ => Some(
-                    self.parse_expr_stmt()
-                        .expect("Failed to parse recursive expression parser."),
-                ),
+                Token::Let => self.parse_let_stmt().unwrap(),
+                Token::Ret => self.parse_ret_stmt().unwrap(),
+                _ => self.parse_expr_stmt().unwrap(),
             }
         }
 
-        pub fn parse_let_stmt(&mut self) -> Option<Stmt> {
-            if let Token::Ident(_) = &self.next_token {
-                self.next_token();
-            } else {
-                return None;
-            }
-            let ident = match self.parse_ident() {
-                Some(ident) => ident,
-                None => return None,
-            };
+        pub fn parse_let_stmt(&mut self) -> Result<Stmt> {
+            self.next_token();
+            let ident = self.parse_ident()?;
             self.next_token_if_is(Token::Assign);
             self.next_token();
-            let lit = match self.parse_expr() {
-                Some(expr) => expr,
-                None => return None,
-            };
+            let literal = self.parse_expr()?;
             self.next_token_if_is(Token::Semicolon);
-            Some(Stmt::Let(Expression::ExprIdent(ident), lit))
+            Ok(Stmt::Let(Expression::ExprIdent(ident), literal))
         }
 
-        pub fn parse_ret_stmt(&mut self) -> Option<Stmt> {
+        pub fn parse_ret_stmt(&mut self) -> Result<Stmt> {
             self.next_token();
-            let ret_expr = match self.parse_expr() {
-                Some(expr) => expr,
-                None => return None,
-            };
+            let ret_expr = self.parse_expr()?;
             self.next_token_if_is(Token::Semicolon);
-            Some(Stmt::Return(ret_expr))
+            Ok(Stmt::Return(ret_expr))
         }
 
-        pub fn parse_expr_stmt(&mut self) -> Option<Stmt> {
-            let l_val = self
-                .parse_expr()
-                .expect("Failed to parse l val in expr stmt.");
-            if let Some(_) = self.parse_operator() {
-                if let Some(infix) = self.parse_infix() {
-                    return Some(Stmt::ExprStmt(l_val, Some(infix)));
-                } else {
-                    None
+        pub fn parse_expr_stmt(&mut self) -> Result<Stmt> {
+            let l_val = self.parse_expr()?;
+            while self.is_operator() {
+                if let Ok(infix) = self.parse_infix() {
+                    self.next_token_if_is(Token::Semicolon);
+                    return Ok(Stmt::ExprStmt(l_val, Some(infix)));
                 }
-            } else {
-                self.next_token_if_is(Token::Semicolon);
-                return Some(Stmt::ExprStmt(l_val, None))
             }
+            self.next_token_if_is(Token::Semicolon);
+            Ok(Stmt::ExprStmt(l_val, None))
         }
 
-        pub fn parse_infix(&mut self) -> Option<Expression> {
-            let operator = match self.parse_operator() {
-                Some(op) => op,
-                None => return None,
+        pub fn parse_infix(&mut self) -> Result<Expression> {
+            let operator = self.parse_operator()?;
+            self.next_token();
+            let r_val = self.parse_expr()?;
+            Ok(Expression::Infix(operator, Box::new(r_val)))
+        }
+
+        pub fn parse_prefix(&mut self) -> Result<Expression> {
+            let prefix = match self.cur_token {
+                Token::Minus => PrefixOp::Negative,
+                Token::Bang => PrefixOp::NotEq,
+                Token::Plus => PrefixOp::Positive,
+                _ => return Err(Box::new(MonkeyError::new(get_fn_name!()))),
             };
             self.next_token();
-            let r_val = match self.parse_expr() {
-                Some(val) => val,
-                None => return None,
-            };
-            Some(Expression::Infix(operator, Box::new(r_val)))
+            let expr = self.parse_expr()?;
+            Ok(Expression::Prefix(prefix, Box::new(expr)))
         }
 
-        pub fn parse_expr(&mut self) -> Option<Expression> {
+        pub fn parse_expr(&mut self) -> Result<Expression> {
             let l_val = match self.cur_token {
-                Token::Int(_) => Some(Expression::Literal(
-                    self.parse_int()
-                        .expect("Failed to parse int in parse expr."),
-                )),
-                Token::Float(_) => Some(Expression::Literal(
-                    self.parse_float()
-                        .expect("Failed to parse float in parse expr."),
-                )),
-                Token::True(_) | Token::False(_) => Some(Expression::Literal(
-                    self.parse_bool()
-                        .expect("Failed to parse bool in parse expr."),
-                )),
-
-                Token::Ident(_) => Some(Expression::ExprIdent(
-                    self.parse_ident()
-                        .expect("Failed to parse ident in parse expr."),
-                )),
-                _ => return None,
+                Token::Int(_) => Ok(Expression::Literal(self.parse_int()?)),
+                Token::Float(_) => Ok(Expression::Literal(self.parse_float()?)),
+                Token::True(_) | Token::False(_) => Ok(Expression::Literal(self.parse_bool()?)),
+                Token::Ident(_) => Ok(Expression::ExprIdent(self.parse_ident()?)),
+                Token::Plus | Token::Minus | Token::Bang => Ok(self.parse_prefix()?),
+                _ => {
+                    return Err(Box::new(ErrorType::InvalidToken(
+                        Box::new(MonkeyError::new(get_fn_name!())),
+                        Some(self.cur_token.clone()),
+                    )))
+                }
             };
-            if !self.next_token_is(&Token::Semicolon) {
-                self.next_token();
-            }
+
+            self.next_token();
             l_val
         }
 
-        pub fn parse_operator(&mut self) -> Option<Operator> {
+        pub fn parse_operator(&mut self) -> Result<Operator> {
             match self.cur_token {
-                Token::Plus => Some(Operator::Plus),
-                Token::Minus => Some(Operator::Minus),
-                Token::Asterisk => Some(Operator::Multiply),
-                Token::Slash => Some(Operator::Divide),
-                Token::Eq => Some(Operator::Eq),
-                Token::NotEq => Some(Operator::NotEq),
-                Token::Lt => Some(Operator::Lt),
-                Token::Gt => Some(Operator::Gt),
-                Token::LtEq => Some(Operator::LtEq),
-                Token::GtEq => Some(Operator::GtEq),
-                _ => None,
+                Token::Plus => Ok(Operator::Plus),
+                Token::Minus => Ok(Operator::Minus),
+                Token::Asterisk => Ok(Operator::Multiply),
+                Token::Slash => Ok(Operator::Divide),
+                Token::Eq => Ok(Operator::Eq),
+                Token::NotEq => Ok(Operator::NotEq),
+                Token::Lt => Ok(Operator::Lt),
+                Token::Gt => Ok(Operator::Gt),
+                Token::LtEq => Ok(Operator::LtEq),
+                Token::GtEq => Ok(Operator::GtEq),
+                _ => return Err(Box::new(MonkeyError::new(get_fn_name!()))),
             }
         }
 
-        pub fn parse_ident(&mut self) -> Option<Ident> {
+        pub fn parse_ident(&mut self) -> Result<Ident> {
             match self.cur_token {
-                Token::Ident(ref ident) => Some(Ident(ident.to_string())),
-                _ => None,
+                Token::Ident(ref ident) => Ok(Ident(ident.to_string())),
+                _ => Err(Box::new(MonkeyError::new(get_fn_name!()))),
             }
         }
 
-        pub fn parse_float(&mut self) -> Option<Type> {
+        pub fn parse_float(&mut self) -> Result<Type> {
             match self.cur_token {
-                Token::Float(num) => Some(Type::Float(num)),
-                _ => None,
+                Token::Float(num) => Ok(Type::Float(num)),
+                _ => Err(Box::new(MonkeyError::new(get_fn_name!()))),
             }
         }
 
-        pub fn parse_bool(&mut self) -> Option<Type> {
+        pub fn parse_bool(&mut self) -> Result<Type> {
             match self.cur_token {
-                Token::True(val) | Token::False(val) => Some(Type::Bool(val)),
-                _ => None,
+                Token::True(val) | Token::False(val) => Ok(Type::Bool(val)),
+                _ => Err(Box::new(MonkeyError::new(get_fn_name!()))),
             }
         }
 
-        pub fn parse_int(&mut self) -> Option<Type> {
+        pub fn parse_int(&mut self) -> Result<Type> {
             match self.cur_token {
-                Token::Int(num) => Some(Type::Int(num)),
-                _ => None,
+                Token::Int(num) => Ok(Type::Int(num)),
+                _ => Err(Box::new(MonkeyError::new(get_fn_name!()))),
             }
         }
     }
@@ -256,11 +311,52 @@ mod tests {
         );
     }
     #[test]
+    fn test_parse_prefix() {
+        let tests = vec![
+            (
+                "-5",
+                Stmt::ExprStmt(
+                    Expression::Prefix(
+                        PrefixOp::Negative,
+                        Box::new(Expression::Literal(Type::Int(5))),
+                    ),
+                    None,
+                ),
+            ),
+            (
+                "+3",
+                Stmt::ExprStmt(
+                    Expression::Prefix(
+                        PrefixOp::Positive,
+                        Box::new(Expression::Literal(Type::Int(3))),
+                    ),
+                    None,
+                ),
+            ),
+            (
+                "!1",
+                Stmt::ExprStmt(
+                    Expression::Prefix(
+                        PrefixOp::NotEq,
+                        Box::new(Expression::Literal(Type::Int(1))),
+                    ),
+                    None,
+                ),
+            ),
+        ];
+        for (input, expect) in tests {
+            let mut parser = Parser::new(Lexer::new(input));
+            let program = parser.parse();
+            assert_eq!(vec![expect], program);
+        }
+    }
+
+    #[test]
     fn test_return_stmts() {
         let input = r#"
     return 37;
     return x;
-"#;
+    "#;
 
         let mut parser = Parser::new(Lexer::new(input));
         let program = parser.parse();
@@ -276,13 +372,34 @@ mod tests {
     fn test_expr_stmts() {
         let tests = vec![
             (
+                "x + 5;",
+                Stmt::ExprStmt(
+                    Expression::ExprIdent(Ident("x".to_string())),
+                    Some(Expression::Infix(
+                        Operator::Plus,
+                        Box::new(Expression::Literal(Type::Int(5))),
+                    )),
+                ),
+            ),
+            (
+                "x + x;",
+                Stmt::ExprStmt(
+                    Expression::ExprIdent(Ident("x".to_string())),
+                    Some(Expression::Infix(
+                        Operator::Plus,
+                        Box::new(Expression::ExprIdent(Ident("x".to_string()))),
+                    )),
+                ),
+            ),
+            (
                 "1 + 5;",
                 Stmt::ExprStmt(
                     Expression::Literal(Type::Int(1)),
                     Some(Expression::Infix(
                         Operator::Plus,
                         Box::new(Expression::Literal(Type::Int(5))),
-                    ))),
+                    )),
+                ),
             ),
             (
                 "1 - 5;",
@@ -291,7 +408,8 @@ mod tests {
                     Some(Expression::Infix(
                         Operator::Minus,
                         Box::new(Expression::Literal(Type::Int(5))),
-                    ))),
+                    )),
+                ),
             ),
             (
                 "1 * 5;",
@@ -300,7 +418,8 @@ mod tests {
                     Some(Expression::Infix(
                         Operator::Multiply,
                         Box::new(Expression::Literal(Type::Int(5))),
-                    ))),
+                    )),
+                ),
             ),
             (
                 "1 / 5;",
@@ -309,7 +428,8 @@ mod tests {
                     Some(Expression::Infix(
                         Operator::Divide,
                         Box::new(Expression::Literal(Type::Int(5))),
-                    ))),
+                    )),
+                ),
             ),
             (
                 "1 > 5;",
@@ -318,7 +438,8 @@ mod tests {
                     Some(Expression::Infix(
                         Operator::Gt,
                         Box::new(Expression::Literal(Type::Int(5))),
-                    ))),
+                    )),
+                ),
             ),
             (
                 "1 < 5;",
@@ -327,7 +448,8 @@ mod tests {
                     Some(Expression::Infix(
                         Operator::Lt,
                         Box::new(Expression::Literal(Type::Int(5))),
-                    ))),
+                    )),
+                ),
             ),
             (
                 "1 == 5;",
@@ -336,7 +458,8 @@ mod tests {
                     Some(Expression::Infix(
                         Operator::Eq,
                         Box::new(Expression::Literal(Type::Int(5))),
-                    ))),
+                    )),
+                ),
             ),
             (
                 "1 != 5;",
@@ -345,7 +468,8 @@ mod tests {
                     Some(Expression::Infix(
                         Operator::NotEq,
                         Box::new(Expression::Literal(Type::Int(5))),
-                    ))),
+                    )),
+                ),
             ),
             (
                 "1 >= 5;",
@@ -354,7 +478,8 @@ mod tests {
                     Some(Expression::Infix(
                         Operator::GtEq,
                         Box::new(Expression::Literal(Type::Int(5))),
-                    ))),
+                    )),
+                ),
             ),
             (
                 "1 <= 5;",
@@ -363,7 +488,8 @@ mod tests {
                     Some(Expression::Infix(
                         Operator::LtEq,
                         Box::new(Expression::Literal(Type::Int(5))),
-                    ))),
+                    )),
+                ),
             ),
             (
                 "3;",
@@ -374,6 +500,18 @@ mod tests {
             let mut parser = Parser::new(Lexer::new(input));
             let program = parser.parse();
             assert_eq!(vec![expect], program);
+        }
+    }
+    #[test]
+    fn test_err_handling() {
+        let tests = vec![
+            ("!", "Parser Error in monkey::parser::monkey_parser::Parser::parse_expr. Token::Eof is Invalid."),
+        ];
+        for (input, expect) in tests {
+            let mut parser = Parser::new(Lexer::new(input));
+            if let Err(err) = parser.parse_expr() {
+                assert_eq!(expect, err.to_string());
+            };
         }
     }
 }
